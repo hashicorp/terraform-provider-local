26dc0a93434f95809e527d20f1d0ef8744d482cc
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-03-07 19:02:39 +0000

    new `resource/local_sensitive_file`

---
+++ internal/provider/resource_local_sensitive_file.go
@@ -0,0 +1,79 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceLocalSensitiveFile() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLocalSensitiveFileCreate,
+		Read:   resourceLocalSensitiveFileRead,
+		Delete: resourceLocalSensitiveFileDelete,
+
+		Description: "Generates a local file with the given sensitive content.",
+
+		Schema: map[string]*schema.Schema{
+			"content": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Sensitive:    true,
+				ExactlyOneOf: []string{"content", "content_base64", "source"},
+				Description:  "Sensitive content to store in the file, expected to be an UTF-8 encoded string.",
+			},
+			"content_base64": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Sensitive:    true,
+				ExactlyOneOf: []string{"content", "content_base64", "source"},
+				Description:  "Sensitive content to store in the file, expected to be binary encoded as base64 string.",
+			},
+			"source": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ExactlyOneOf: []string{"content", "content_base64", "source"},
+				Description:  "Path to file to use as source for the one we are creating.",
+			},
+			"filename": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				Description: `
+					The path to the file that will be created.
+					Missing parent directories will be created.
+					If the file already exists, it will be overridden with the given content.
+				`,
+			},
+			"file_permission": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Default:      "0777",
+				ValidateFunc: validateModePermission,
+				Description:  "Permissions to set for the output file (in numeric notation).",
+			},
+			"directory_permission": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Default:      "0777",
+				ValidateFunc: validateModePermission,
+				Description:  "Permissions to set for directories created (in numeric notation).",
+			},
+		},
+	}
+}
+
+func resourceLocalSensitiveFileRead(d *schema.ResourceData, m interface{}) error {
+	return resourceLocalFileRead(d, m)
+}
+
+func resourceLocalSensitiveFileCreate(d *schema.ResourceData, m interface{}) error {
+	return resourceLocalFileCreate(d, m)
+}
+
+func resourceLocalSensitiveFileDelete(d *schema.ResourceData, m interface{}) error {
+	return resourceLocalFileDelete(d, m)
+}
---
+++ internal/provider/resource_local_sensitive_file_test.go
@@ -0,0 +1,216 @@
+package provider
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+
+	r "github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestLocalSensitiveFile_Basic(t *testing.T) {
+	td := createTestingTempDir(t)
+	defer os.RemoveAll(td)
+
+	f := filepath.Join(td, "local_file")
+	f = strings.ReplaceAll(f, `\`, `\\`)
+
+	var cases = []struct {
+		path    string
+		content string
+		config  string
+	}{
+		{
+			f,
+			"This is some content", fmt.Sprintf(`
+				resource "local_file" "file" {
+				  content  = "This is some content"
+				  filename = "%s"
+				}`, f,
+		),
+		},
+		{
+			f,
+			"This is some sensitive content", fmt.Sprintf(`
+				resource "local_file" "file" {
+				  sensitive_content = "This is some sensitive content"
+				  filename = "%s"
+				}`, f,
+		),
+		},
+		{
+			f,
+			"This is some base64 content", fmt.Sprintf(`
+				resource "local_file" "file" {
+				  content_base64 = "VGhpcyBpcyBzb21lIGJhc2U2NCBjb250ZW50"
+				  filename = "%s"
+				}`, f,
+		),
+		},
+		{
+			f,
+			"This is some base64 content", fmt.Sprintf(`
+				resource "local_file" "file" {
+				  content_base64 = base64encode("This is some base64 content")
+				  filename = "%s"
+				}`, f,
+		),
+		},
+	}
+
+	for i, tt := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			r.UnitTest(t, r.TestCase{
+				Providers: testProviders,
+				Steps: []r.TestStep{
+					{
+						Config: tt.config,
+						Check: func(s *terraform.State) error {
+							content, err := ioutil.ReadFile(tt.path)
+							if err != nil {
+								return fmt.Errorf("config:\n%s\n,got: %s\n", tt.config, err)
+							}
+							if string(content) != tt.content {
+								return fmt.Errorf("config:\n%s\ngot:\n%s\nwant:\n%s\n", tt.config, content, tt.content)
+							}
+							return nil
+						},
+					},
+				},
+				CheckDestroy: func(*terraform.State) error {
+					if _, err := os.Stat(tt.path); os.IsNotExist(err) {
+						return nil
+					}
+					return errors.New("local_file did not get destroyed")
+				},
+			})
+		})
+	}
+}
+
+func TestLocalSensitiveFile_source(t *testing.T) {
+	tmp, err := ioutil.TempDir("", "local-file-source")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmp)
+
+	// create a local file that will be used as the "source" file
+	source_content := "local file content"
+	if err := ioutil.WriteFile("source_file", []byte(source_content), 0644); err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove("source_file")
+
+	config := `
+		resource "local_file" "file" {
+		  source = "source_file"
+		  filename = "new_file"
+		}
+	`
+
+	r.UnitTest(t, r.TestCase{
+		Providers: testProviders,
+		Steps: []r.TestStep{
+			{
+				Config: config,
+				Check: func(s *terraform.State) error {
+					content, err := ioutil.ReadFile("new_file")
+					if err != nil {
+						return fmt.Errorf("config:\n%s\n,got: %s\n", config, err)
+					}
+					if string(content) != source_content {
+						return fmt.Errorf("config:\n%s\ngot:\n%s\nwant:\n%s\n", config, content, source_content)
+					}
+					return nil
+				},
+			},
+		},
+		CheckDestroy: func(*terraform.State) error {
+			if _, err := os.Stat("new_file"); os.IsNotExist(err) {
+				return nil
+			}
+			return errors.New("local_file did not get destroyed")
+		},
+	})
+}
+
+func TestLocalSensitiveFile_Permissions(t *testing.T) {
+	td := createTestingTempDir(t)
+	defer os.RemoveAll(td)
+
+	destinationDirPath := td
+	destinationFilePath := filepath.Join(destinationDirPath, "local_file")
+	destinationFilePath = strings.ReplaceAll(destinationFilePath, `\`, `\\`)
+	filePermission := os.FileMode(0600)
+	directoryPermission := os.FileMode(0700)
+	skipDirCheck := false
+	config := fmt.Sprintf(`
+		resource "local_file" "file" {
+			content              = "This is some content"
+			filename             = "%s"
+			file_permission      = "0600"
+			directory_permission = "0700"
+		}`, destinationFilePath,
+	)
+
+	r.UnitTest(t, r.TestCase{
+		Providers: testProviders,
+		Steps: []r.TestStep{
+			{
+				Config: config,
+				PreConfig: func() {
+					// if directory already existed prior to check, skip check
+					if _, err := os.Stat(path.Dir(destinationFilePath)); !os.IsNotExist(err) {
+						skipDirCheck = true
+					}
+				},
+				Check: func(s *terraform.State) error {
+					if runtime.GOOS == "windows" {
+						// skip all checks if windows
+						return nil
+					}
+
+					fileInfo, err := os.Stat(destinationFilePath)
+					if err != nil {
+						return fmt.Errorf("config:\n%s\ngot:%s\n", config, err)
+					}
+
+					if fileInfo.Mode() != filePermission {
+						return fmt.Errorf(
+							"File permission.\nconfig:\n%s\nexpected:%s\ngot: %s\n",
+							config, filePermission, fileInfo.Mode())
+					}
+
+					if !skipDirCheck {
+						dirInfo, _ := os.Stat(path.Dir(destinationFilePath))
+						// we have to use FileMode.Perm() here, otherwise directory bit causes issues
+						if dirInfo.Mode().Perm() != directoryPermission.Perm() {
+							return fmt.Errorf(
+								"Directory permission.\nconfig:\n%s\nexpected:%s\ngot: %s\n",
+								config, directoryPermission, dirInfo.Mode().Perm())
+						}
+					}
+
+					return nil
+				},
+			},
+		},
+		CheckDestroy: func(*terraform.State) error {
+			if _, err := os.Stat(destinationFilePath); os.IsNotExist(err) {
+				return nil
+			}
+			return errors.New("local_file did not get destroyed")
+		},
+	})
+
+	defer os.Remove(destinationDirPath)
+
+}
