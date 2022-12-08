7b19559f53392d31cee1053a9e21fd6e5ec6d4b2
SBGoods <github@simplebox.anonaddy.com>
2022-12-08 16:29:10 -0500

    Refactor tests

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -2,7 +2,10 @@ package provider
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
+	"runtime"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
@@ -17,10 +20,83 @@ func protoV5ProviderFactories() map[string]func() (tfprotov5.ProviderServer, err
 }
 
 func checkFileDeleted(shouldNotExistFile string) resource.TestCheckFunc {
-	return func(*terraform.State) error {
+	return func(s *terraform.State) error {
 		if _, err := os.Stat(shouldNotExistFile); os.IsNotExist(err) {
 			return nil
 		}
 		return fmt.Errorf("file %s was not deleted", shouldNotExistFile)
+	}
+}
+
+func checkFileCreation(resourceName, path string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resultContent, err := ioutil.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("Error occured while reading file at path: %s\n, error: %s\n", path, err)
+		}
+
+		resource.TestCheckResourceAttr(resourceName, "content", string(resultContent))
+
+		return nil
+	}
+}
+
+func checkFilePermissions(destinationFilePath string, filePermission os.FileMode) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if runtime.GOOS == "windows" {
+			// skip all checks if windows
+			return nil
+		}
+
+		fileInfo, err := os.Stat(destinationFilePath)
+		if err != nil {
+			return fmt.Errorf("Error occured while retrieving file info at path: %s\n, error: %s\n",
+				destinationFilePath, err)
+		}
+
+		if fileInfo.Mode() != filePermission {
+			return fmt.Errorf(
+				"File permission.\nexpected:%s\ngot: %s\n",
+				filePermission, fileInfo.Mode())
+		}
+
+		return nil
+	}
+}
+
+func checkDirectoryPermissions(destinationFilePath string, directoryPermission os.FileMode) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		dirInfo, _ := os.Stat(path.Dir(destinationFilePath))
+		// we have to use FileMode.Perm() here, otherwise directory bit causes issues
+		if dirInfo.Mode().Perm() != directoryPermission.Perm() {
+			return fmt.Errorf(
+				"Directory permission. \nexpected:%s\ngot: %s\n",
+				directoryPermission, dirInfo.Mode().Perm())
+		}
+
+		return nil
+	}
+}
+
+func createSourceFile(sourceContent string) error {
+	return ioutil.WriteFile("source_file", []byte(sourceContent), 0644)
+}
+
+func checkDirExists(destinationFilePath string, isDirExist *bool) func() {
+	return func() {
+		// if directory already existed prior to check, skip check
+		if _, err := os.Stat(path.Dir(destinationFilePath)); !os.IsNotExist(err) {
+			*isDirExist = true
+		}
+	}
+}
+
+func skipTestsWindows() func() (bool, error) {
+	return func() (bool, error) {
+		if runtime.GOOS == "windows" {
+			// skip all checks if windows
+			return true, nil
+		}
+		return false, nil
 	}
 }
--- internal/provider/resource_local_sensitive_file_test.go
+++ internal/provider/resource_local_sensitive_file_test.go
@@ -2,111 +2,57 @@ package provider
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path"
 	"path/filepath"
-	"runtime"
+	"regexp"
 	"strings"
 	"testing"
 
 	r "github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestLocalSensitiveFile_Basic(t *testing.T) {
 	f := filepath.Join(t.TempDir(), "local_sensitive_file")
 	f = strings.ReplaceAll(f, `\`, `\\`)
 
-	var cases = []struct {
-		path    string
-		content string
-		config  string
-	}{
-		{
-			f,
-			"This is some sensitive content", fmt.Sprintf(`
-				resource "local_sensitive_file" "file" {
-				  content  = "This is some sensitive content"
-				  filename = "%s"
-				}`, f,
-			),
+	r.UnitTest(t, r.TestCase{
+		ProtoV5ProviderFactories: protoV5ProviderFactories(),
+		Steps: []r.TestStep{
+			{
+				Config: testAccConfigLocalSensitiveFileContent("This is some sensitive content", f),
+				Check:  checkFileCreation("local_sensitive_file_resource.test", f),
+			},
+			{
+				Config: testAccConfigLocalSensitiveFileEncodedBase64Content("VGhpcyBpcyBzb21lIGJhc2U2NCBjb250ZW50", f),
+				Check:  checkFileCreation("local_sensitive_file_resource.test", f),
+			},
+			{
+				Config: testAccConfigLocalSensitiveFileDecodedBase64Content("This is some base64 content", f),
+				Check:  checkFileCreation("local_sensitive_file_resource.test", f),
+			},
 		},
-		{
-			f,
-			"This is some sensitive base64 content", fmt.Sprintf(`
-				resource "local_sensitive_file" "file" {
-				  content_base64 = "VGhpcyBpcyBzb21lIHNlbnNpdGl2ZSBiYXNlNjQgY29udGVudA=="
-				  filename = "%s"
-				}`, f,
-			),
-		},
-		{
-			f,
-			"This is some sensitive base64 content", fmt.Sprintf(`
-				resource "local_sensitive_file" "file" {
-				  content_base64 = base64encode("This is some sensitive base64 content")
-				  filename = "%s"
-				}`, f,
-			),
-		},
-	}
-
-	for i, tt := range cases {
-		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			r.UnitTest(t, r.TestCase{
-				ProtoV5ProviderFactories: protoV5ProviderFactories(),
-				Steps: []r.TestStep{
-					{
-						Config: tt.config,
-						Check: func(s *terraform.State) error {
-							content, err := ioutil.ReadFile(tt.path)
-							if err != nil {
-								return fmt.Errorf("config:\n%s\n,got: %s\n", tt.config, err)
-							}
-							if string(content) != tt.content {
-								return fmt.Errorf("config:\n%s\ngot:\n%s\nwant:\n%s\n", tt.config, content, tt.content)
-							}
-							return nil
-						},
-					},
-				},
-				CheckDestroy: checkFileDeleted(tt.path),
-			})
-		})
-	}
+		CheckDestroy: checkFileDeleted(f),
+	})
 }
 
 func TestLocalSensitiveFile_source(t *testing.T) {
 	// create a local file that will be used as the "source" file
-	source_content := "local file content"
-	if err := ioutil.WriteFile("source_file", []byte(source_content), 0644); err != nil {
+	if err := createSourceFile("local file content"); err != nil {
 		t.Fatal(err)
 	}
 	defer os.Remove("source_file")
-
-	config := `
-		resource "local_sensitive_file" "file" {
-		  source = "source_file"
-		  filename = "new_file"
-		}
-	`
 
 	r.UnitTest(t, r.TestCase{
 		ProtoV5ProviderFactories: protoV5ProviderFactories(),
 		Steps: []r.TestStep{
 			{
-				Config: config,
-				Check: func(s *terraform.State) error {
-					content, err := ioutil.ReadFile("new_file")
-					if err != nil {
-						return fmt.Errorf("config:\n%s\n,got: %s\n", config, err)
+				Config: `
+					resource "local_sensitive_file" "file" {
+					  source = "source_file"
+					  filename = "new_file"
 					}
-					if string(content) != source_content {
-						return fmt.Errorf("config:\n%s\ngot:\n%s\nwant:\n%s\n", config, content, source_content)
-					}
-					return nil
-				},
+				`,
+				Check: checkFileCreation("local_sensitive_file_resource.test", "new_file"),
 			},
 		},
 		CheckDestroy: checkFileDeleted("new_file"),
@@ -119,58 +65,84 @@ func TestLocalSensitiveFile_Permissions(t *testing.T) {
 	destinationFilePath = strings.ReplaceAll(destinationFilePath, `\`, `\\`)
 	filePermission := os.FileMode(0600)
 	directoryPermission := os.FileMode(0700)
-	skipDirCheck := false
-	config := fmt.Sprintf(`
-		resource "local_sensitive_file" "file" {
-			content              = "This is some content"
-			filename             = "%s"
-			file_permission      = "0600"
-			directory_permission = "0700"
-		}`, destinationFilePath,
-	)
+	isDirExist := false
 
 	r.UnitTest(t, r.TestCase{
 		ProtoV5ProviderFactories: protoV5ProviderFactories(),
 		Steps: []r.TestStep{
 			{
-				Config: config,
-				PreConfig: func() {
-					// if directory already existed prior to check, skip check
-					if _, err := os.Stat(path.Dir(destinationFilePath)); !os.IsNotExist(err) {
-						skipDirCheck = true
-					}
-				},
-				Check: func(s *terraform.State) error {
-					if runtime.GOOS == "windows" {
-						// skip all checks if windows
-						return nil
-					}
-
-					fileInfo, err := os.Stat(destinationFilePath)
-					if err != nil {
-						return fmt.Errorf("config:\n%s\ngot:%s\n", config, err)
-					}
-
-					if fileInfo.Mode() != filePermission {
-						return fmt.Errorf(
-							"File permission.\nconfig:\n%s\nexpected:%s\ngot: %s\n",
-							config, filePermission, fileInfo.Mode())
-					}
-
-					if !skipDirCheck {
-						dirInfo, _ := os.Stat(path.Dir(destinationFilePath))
-						// we have to use FileMode.Perm() here, otherwise directory bit causes issues
-						if dirInfo.Mode().Perm() != directoryPermission.Perm() {
-							return fmt.Errorf(
-								"Directory permission.\nconfig:\n%s\nexpected:%s\ngot: %s\n",
-								config, directoryPermission, dirInfo.Mode().Perm())
-						}
-					}
-
-					return nil
-				},
+				PreConfig: checkDirExists(destinationDirPath, &isDirExist),
+				SkipFunc:  skipTestsWindows(),
+				Config: fmt.Sprintf(`
+					resource "local_sensitive_file" "file" {
+						content              = "This is some content"
+						filename             = "%s"
+						file_permission      = "0600"
+						directory_permission = "0700"
+					}`, destinationFilePath,
+				),
+				Check: r.ComposeTestCheckFunc(
+					checkFilePermissions(destinationFilePath, filePermission),
+					checkDirectoryPermissions(destinationFilePath, directoryPermission)),
 			},
+		},
+		ErrorCheck: func(err error) error {
+			if match, _ := regexp.MatchString("Directory permission.", err.Error()); match && isDirExist {
+				return nil
+			}
+			return err
 		},
 		CheckDestroy: checkFileDeleted(destinationFilePath),
 	})
+}
+
+func TestLocalSensitiveFile_Validators(t *testing.T) {
+	f := filepath.Join(t.TempDir(), "local_file")
+
+	r.UnitTest(t, r.TestCase{
+		ProtoV5ProviderFactories: protoV5ProviderFactories(),
+		CheckDestroy:             nil,
+		Steps: []r.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				resource "local_sensitive_file" "file" {
+				  filename = "%s"
+				}`, f),
+				ExpectError: regexp.MustCompile(`.*Error: Invalid Attribute Combination`),
+			},
+			{
+				Config: fmt.Sprintf(`
+				resource "local_sensitive_file" "file" {
+                  content = "content"
+				  content_base64 = "VGhpcyBpcyBzb21lIGJhc2U2NCBjb250ZW50"
+				  filename = "%s"
+				}`, f),
+				ExpectError: regexp.MustCompile(`.*Error: Invalid Attribute Combination`),
+			},
+		},
+	})
+}
+
+func testAccConfigLocalSensitiveFileContent(content, filename string) string {
+	return fmt.Sprintf(`
+				resource "local_sensitive_file" "file" {
+				  content  = %[1]q
+				  filename = %[2]q
+				}`, content, filename)
+}
+
+func testAccConfigLocalSensitiveFileEncodedBase64Content(content, filename string) string {
+	return fmt.Sprintf(`
+				resource "local_sensitive_file" "file" {
+				  content_base64  = %[1]q
+				  filename = %[2]q
+				}`, content, filename)
+}
+
+func testAccConfigLocalSensitiveFileDecodedBase64Content(content, filename string) string {
+	return fmt.Sprintf(`
+				resource "local_sensitive_file" "file" {
+				  content_base64  = base64encode(%[1]q)
+				  filename = %[2]q
+				}`, content, filename)
 }
