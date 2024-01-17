0a2a4c3b837c8633868bc2f4fa267d85fa7d18da
Austin Valle <austinvalle@gmail.com>
2024-01-17 15:12:36 -0500

    add license headers

--- internal/provider/testdata/TestLocalFileDataSource/main.tf
+++ internal/provider/testdata/TestLocalFileDataSource/main.tf
@@ -1,3 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 data "local_file" "file" {
   # Known issue where relative path is based on where the test working directory is located:
   # https://github.com/hashicorp/terraform-plugin-testing/issues/277
