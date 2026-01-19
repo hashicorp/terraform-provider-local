5ab7e6e60b262f33f4e51b5e877da6a2b75b965b
Arthur Busser <arthur.busser@gopigment.com>
2026-01-19 17:42:02 +0100

    add resource (no implementation yet)

---
+++ internal/provider/ephemeral_local_command.go
@@ -0,0 +1,27 @@
+// Copyright IBM Corp. 2017, 2025
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
+)
+
+var _ ephemeral.EphemeralResource = (*localCommandEphemeral)(nil)
+
+func NewLocalCommandEphemeral() ephemeral.EphemeralResource {
+	return &localCommandEphemeral{}
+}
+
+type localCommandEphemeral struct{}
+
+func (e *localCommandEphemeral) Metadata(ctx context.Context, req ephemeral.MetadataRequest, resp *ephemeral.MetadataResponse) {
+}
+
+func (e *localCommandEphemeral) Schema(ctx context.Context, req ephemeral.SchemaRequest, resp *ephemeral.SchemaResponse) {
+}
+
+func (e *localCommandEphemeral) Open(ctx context.Context, req ephemeral.OpenRequest, resp *ephemeral.OpenResponse) {
+}
